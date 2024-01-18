46f4887b7fcf241856841bd1d9560a933c7ae5eb
ClaudioAlbertoSica <ClaudioAlbertoSica@gmail.com>
2024-01-17 22:16:52 -0300

    Controller translated

--- controller/controller.js
+++ controller/controller.js
@@ -5,25 +5,25 @@ class Controller {
         this.service = new Service()
     }
 
-    saludo = (req, res) => {
-        const respuesta = this.service.saludo()
-        res.send(`<h1 style="color: blue;">${respuesta}</h1>`)
+    salute = (req, res) => {
+        const response = this.service.salute()
+        res.send(`<h1 style="color: blue;">${response}</h1>`)
     }
 
-    listarTodos = async (req, res) => {
+    listAll = async (req, res) => {
         try{
-        const listado = await this.service.listarTodos()
-        res.json(listado)
+        const list = await this.service.listAll()
+        res.json(list)
     } catch(error){
         res.status(500).send(error.message)
     }
     }
 
-    aniadirColor = async (req, res) => {
+    addColor = async (req, res) => {
         try{
         const { color } = req.body
-        const colorniadido = await this.service.aniadirColor(color)
-        res.json(colorniadido)
+        const addedColor = await this.service.addColor(color)
+        res.json(addedColor)
     } catch(error){
         res.status(500).send(error.message)
     }
@@ -32,19 +32,19 @@ class Controller {
     deleteColor = async (req, res) => {
         try{
         const { color } = req.body
-        const colorBorrado = await this.service.deleteColor(color)
-        res.json(colorBorrado)
+        const deletedColor = await this.service.deleteColor(color)
+        res.json(deletedColor)
     } catch(error){
         res.status(500).send(error.message)
     }
     }
 
 
-    rutaNoAsignada = (req, res) => {
+    NonAssignedRoute = (req, res) => {
         const { baseUrl, method } = req
         res.status(500).send(`<h3 style="color: red;">Error:</h3>
         \n
-        La ruta ${baseUrl}, no está implementada para el método ${method}`)
+        The route ${baseUrl}, is not implemented for the method used by your request: ${method}`)
     }
 }
 
